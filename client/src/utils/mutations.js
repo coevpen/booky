5a91c92cd4d6c3a3cb6c3635ec7dcfa71249fdc9
coevpen <koinuevins@gmail.com>
2022-04-01 17:13:41 -0500

    convertered to apollo and graphql

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        token
+        user {
+            _id
+            username
+        }
+    }
+`;
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: BookInput){
+        saveBook(book: $book){
+            username
+            email
+            bookcount
+            savedBooks{
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!){
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            savedBooks{
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+const {ApolloServer} = require('apollo-server-express');
+const typeDefs = require('./Schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,7 +28,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(_dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
