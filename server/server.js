2db51d297fc0c6d2cb3ea4eaa8a77628426fae53
coevpen <koinuevins@gmail.com>
2022-04-02 16:32:55 -0500

    change to server for heroku

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV) {
-  app.use(express.static(path.resolve(process.cwd(), '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(process.cwd(), '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 
